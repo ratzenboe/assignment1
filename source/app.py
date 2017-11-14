55ab1ddb15e2c5300ca8375aa1d36b2c339dc591
Moritz Brandl <moritz.brandl@live.at>
2017-11-14 12:51:21 +0100

    including elki dataset

--- source/app.py
+++ source/app.py
@@ -4,10 +4,13 @@ Wrapper script for executing PreDeCon on dataset.
 
 from PreDeCon import PreDeCon
 import numpy as np
+import pandas as pd
 import sys
 
 data = np.array([[1, 6], [2, 6], [3, 6], [4, 6], [5, 6], [6, 6], [7, 8], [7, 7], [7, 6], [7, 5], [7, 4], [7, 3]])
 
+elki_data = pd.read_csv("elki_data.csv", delimiter=" ", header=None, names=['x','y','cluster'])
+elki_data = elki_data.drop(elki_data.columns[2], axis=1).values
 
 hyperparameters = dict()
 hyperparameters["mu"] = 3
@@ -17,7 +20,7 @@ hyperparameters["lambda"] = 1
 hyperparameters["kappa"] = 100
 
 # Initiate algorithm.
-predecon = PreDeCon(data=data, hyperparameters=hyperparameters)
+predecon = PreDeCon(data=elki_data, hyperparameters=hyperparameters)
 # Run algorithm.
 clusters = predecon.run()
 print(clusters)
